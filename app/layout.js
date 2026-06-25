4aefed04123a517201f500360aff566ed48677b6
L0vKuro <dalmazank7@gmail.com>
2026-06-25 14:41:02 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,16 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "INK3D — Esport Accesories",
+  title: "INK3D — Esport Accessories",
   description: "3D Printing The Game.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://api.goaffpro.com/loader.js?shop=jczaxqshor" async></script>
+      </head>
       <body>{children}</body>
     </html>
   );
