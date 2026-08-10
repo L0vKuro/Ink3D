29ee2b483b17b959418babebe7f0c2ba3a802f47
rmsm97 <service.ink3dstudio@gmail.com>
2026-08-09 22:43:09 -0400

    Update page.js

--- app/teams/page.js
+++ app/teams/page.js
@@ -145,9 +145,7 @@ const teams = [
     items: [
       { name: "KEYCHAIN", price: "$12.99", tag: "KEYCHAIN", image: "/ninefly-keychain.png" },
       { name: "LIGHTBOX", price: "$49.99", tag: "LIGHTBOX", image: "/ninefly-cubanhypechain.png" },
-      ],
-  },
-  {
+      
 const tagColors = {
   KEYCHAIN:   "text-yellow-400 border-yellow-400/50 bg-yellow-400/10",
   LIGHTBOX:   "text-cyan-400 border-cyan-400/50 bg-cyan-400/10",
