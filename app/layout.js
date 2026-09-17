10c95aeffadabc9c5d3c142d4ee03fc6102414a9
L0vKuro <dalmazank7@gmail.com>
2026-09-16 20:01:40 -0700

    Remove GoAffPro loader script from layout
    Removed the GoAffPro loader script from the layout.

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://api.goaffpro.com/loader.js?shop=jczaxqshor" async></script>
       </head>
       <body>
         <CartProvider>
