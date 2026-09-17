32fbc6567366bc86871e74f9c0ac0e3a48ded000
L0vKuro <dalmazank7@gmail.com>
2026-09-16 19:58:39 -0700

    Update sanitize.js

--- app/lib/sanitize.js
+++ app/lib/sanitize.js
@@ -1,9 +1,9 @@
 export function sanitize(str) {
   if (typeof str !== "string") return str;
   return str
+    .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/&/g, "&amp;")
     .replace(/"/g, "&quot;")
     .replace(/'/g, "&#x27;")
     .replace(/\//g, "&#x2F;")
