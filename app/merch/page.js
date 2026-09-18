fd1de5425662032b14ed207173a7cd3c17a4ddf8
rmsm97 <service.ink3dstudio@gmail.com>
2026-09-17 20:22:10 -0400

    Update page.js

--- app/merch/page.js
+++ app/merch/page.js
@@ -12,7 +12,7 @@ const merch = [
   {
     id: "merch-1",
     name: "INK3D HOODIE",
-    price: "$53.00",
+    price: "COMING SOON",
     tag: "HOODIE",
     desc: "Premium heavyweight hoodie. Wear the brand.",
     hasSize: true,
