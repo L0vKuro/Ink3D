79d2781c4293632451bf0b933e1f8ea88d798156
rmsm97 <service.ink3dstudio@gmail.com>
2026-09-23 18:45:59 -0400

    Rename merch to Ink3d Originals in page.js

--- app/merch/page.js
+++ app/merch/page.js
@@ -8,7 +8,7 @@ import { useCart } from "../context/CartContext";
 
 const sizes = ["XS", "S", "M", "L", "XL", "2XL"];
 
-const merch = [
+const Ink3d Originals = [
   {
     id: "merch-1",
     name: "INK3D HOODIE",
