49ec5d892fb47ea4e25d98623e2309fc3beefe41
rmsm97 <service.ink3dstudio@gmail.com>
2026-09-23 20:22:29 -0400

    Update image path for controller stand item

--- app/originals/page.js
+++ app/originals/page.js
@@ -14,7 +14,7 @@ import { useCart } from "../context/CartContext";
 // 2. Add an entry below, e.g.:
 //    { id: "controller-stand", name: "CONTROLLER STAND", price: "$25.00", tag: "ACCESSORY", image: "/originals-controller-stand.png", credit: "NightStawker" },
 const items = [
-  { id: "controller-stand", name: "CONTROLLER STAND", price: "$25.00", tag: "ACCESSORY", image: "/controller-stand.png", credit: "NightStawker" },
+  { id: "controller-stand", name: "CONTROLLER STAND", price: "$25.00", tag: "ACCESSORY", image: "/Fire-Semblance Controller Holder.jpg", credit: "NightStawker" },
 ];
 
 const tagColors = {
