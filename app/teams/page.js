a84b500d948e05bd150b86021ec8fd786e7d3df1
rmsm97 <service.ink3dstudio@gmail.com>
2026-09-24 21:52:27 -0400

    Update image path for COASTER SET OF 4
    Fixed image path for COASTER SET OF 4 item.

--- app/teams/page.js
+++ app/teams/page.js
@@ -155,7 +155,8 @@ const teams = [
     items: [
      { name: "KEYCHAIN", price: "$5.00", tag: "KEYCHAIN", image: "/7ven keychain.png" }, 
     { name: "LIGHTBOX", price: "$40.00", tag: "LIGHTBOX",
-      { name: "COASTER SET OF 4", price: "$15.00", tag: "COASTER", image: "/7ven coster.png" },  
+     { name: "COASTER SET OF 4", price: "$15.00", tag: "COASTER", image: "/" },
+      
     ] 
   }, 
   { 
