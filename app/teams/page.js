32991ae72172f439594606e56cbd262af5ff1964
rmsm97 <service.ink3dstudio@gmail.com>
2026-09-24 21:19:35 -0400

    Fix syntax error and add images for coaster set
    Fixed syntax error in items array and added images for COASTER SET OF 4.

--- app/teams/page.js
+++ app/teams/page.js
@@ -154,7 +154,14 @@ const teams = [
     color: "#39FF14",
     items: [
      { name: "KEYCHAIN", price: "$5.00", tag: "KEYCHAIN", image: "/7ven keychain.png" }, 
-       { name: "LIGHTBOX", price: "$40.00", tag: "LIGHTBOX" }
+     { name: "LIGHTBOX", price: "$40.00", tag: "LIGHTBOX" }
+      { name: "COASTER SET OF 4", price: "$15.00", tag: "COASTER" },  images: [
+      { label: "FRONT", src: "/" },
+      { label: "BACK", src: "/" },
+      { label: "LEFT", src: "" },
+      { label: "RIGHT", src: "/" },
+
+
     ] 
   }, 
   { 
