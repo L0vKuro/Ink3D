52cbdbf6070261fb72913950338c8d71c5d1d085
rmsm97 <service.ink3dstudio@gmail.com>
2026-09-24 21:37:13 -0400

    Add image details for item in teams page
    Added image information for the item in the items array.

--- app/teams/page.js
+++ app/teams/page.js
@@ -155,7 +155,8 @@ const teams = [
     items: [
      { name: "KEYCHAIN", price: "$5.00", tag: "KEYCHAIN", image: "/7ven keychain.png" }, 
     { name: "LIGHTBOX", price: "$40.00", tag: "LIGHTBOX" },
-      { name: "COASTER SET OF 4", price: "$15.00", tag: "COASTER" },
+      { name: "COASTER SET OF 4", price: "$15.00", tag: "COASTER" },  
+      iamge: { label: "FRONT", src: "/Ink3DHoodie-Front.png" },
     ] 
   }, 
   { 
