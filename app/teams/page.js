5984244c1210ec114f7fe19360b9cbd02027b612
rmsm97 <service.ink3dstudio@gmail.com>
2026-09-24 21:53:55 -0400

    Update page.js

--- app/teams/page.js
+++ app/teams/page.js
@@ -155,8 +155,7 @@ const teams = [
     items: [
      { name: "KEYCHAIN", price: "$5.00", tag: "KEYCHAIN", image: "/7ven keychain.png" }, 
     { name: "LIGHTBOX", price: "$40.00", tag: "LIGHTBOX",
-     { name: "COASTER SET OF 4", price: "$15.00", tag: "COASTER", image: "/" },
-      
+     { name: "COASTER SET OF 4", price: "$15.00", tag: "COASTER", image: "/7ven coster.png" },
     ] 
   }, 
   { 
